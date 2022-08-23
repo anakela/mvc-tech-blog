1e210992b60a4708a7bc56adee47c9bd362589f8
Angie <103882157+anakela@users.noreply.github.com>
2022-08-22 21:29:21 -1000

    Updated index.js to seed.js, and updated the blogData.json and userData.json files.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,16 +1,57 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const { User } = require('../../models');
 
-const {
-    Blog,
-    User
-} = require('../../../models');
-
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const users = await User.findAll({});
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id,
+                req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
     } catch (error) {
         res.status(500).res.json({ error });
+    }
+});
+
+router.post('/sign-in', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: { email: req.body.email },
+        });
+
+        if (!userData) {
+            res.status(400).res.json({ message: `Incorrect email or password.  Please try again.` });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).res.json({ message: `Incorrect email or password.  Please try again.` });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: `Yay!  You've successfully logged in!` });
+        });
+    } catch (error) {
+        res.status(500).res.json({ error });
+    }
+});
+
+router.post('/sign-out', async (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(400).end();
     }
 });
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Pages prepended with /api
 router.use('/api', apiRoutes);
 
 module.exports = router;
